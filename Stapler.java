6a52c4adfb17362aa0de6097cdb7c060afc946a0
Emmett-Hutton <Emmett.Hutton5.SchoolEmail@gmail.com>
2024-09-27 12:22:33 -0500

    Update Stapler.java

--- Stapler.java
+++ Stapler.java
@@ -4,17 +4,39 @@ class Stapler{
     //Data
     private int staples;
     private boolean isOpen;
-		private int capacity;
+	private int capacity;
     
     //Methods (Behaviors)
     public Stapler(){
         staples = 0;
         isOpen = false;
+        capacity = 500;
+    }
+    public Stapler(int numStaples){
+        staples = numStaples;
+        isOpen = false;
+        capacity = 500;
+    }
+    public Stapler(Stapler stapleGuy){
+        staples = stapleGuy.getStaples();
+        isOpen = !stapleGuy.isClosed();
+        capacity = 500;
     }
     
     public void loadStaples(int s){
         //Update staples variable to be s more, but no more than the total capacity (default is 500).
         //If s is not positive, ignore the value
+        if(s>=0){
+            //System.out.println(staples+s + "before cap check");
+            if((staples + s) <= capacity){
+                //System.out.println(staples+s + "after cap check");
+                staples += s;
+            }
+            else{
+                staples=500;
+            }
+        }
+        //System.out.println(getStaples());
         
         
     }
@@ -23,18 +45,19 @@ class Stapler{
         //1.  If the stapler is closed and not out of staples, staple!
         //2.  Reduce the number of staples by 1 & return true.
         //If there was a problem stapling (not closed, out of staples) return false;
-        
+        if(isOpen!=true && staples>0){
+            staples-=1;
+            return true;
+        }
         return false;
     }
     
     public void open(){
-        //Open the stapler
-        
+        isOpen=true;
     }
     
     public void close(){
-        //Close the stapler;
-        
+        isOpen=false;        
     }
     
     public int getStaples(){
@@ -48,6 +71,6 @@ class Stapler{
 	@return returns a phrase like "This stapler has room for x staples" where x is the remaining capacity after accounting for staples in the Stapler
 	**/
 		public String toString(){
-			return "";
+			return "This stapler has room for " + (500-staples) + " staples";
 		}
 }
